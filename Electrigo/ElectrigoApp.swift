503535033fcad845f31d290c2b3f89908c645995
aziz jazzar <jazzar.aziz>
2023-11-04 18:04:23 +0100

    Initial Commit

---
+++ Electrigo/ElectrigoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ElectrigoApp.swift
+//  Electrigo
+//
+//  Created by aziz jazzar on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ElectrigoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
