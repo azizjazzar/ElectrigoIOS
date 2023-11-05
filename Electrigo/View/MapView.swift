b3927b463178129a3812143170d69c85efaf73e7
dhiaaissa70 <dhia.aissa@esprit.tn>
2023-11-05 01:44:50 +0100

    MapKit

---
+++ Electrigo/View/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  Electrigo
+//
+//  Created by Aissa Dhia on 5/11/2023.
+//
+
+import SwiftUI
+import MapKit
+import CoreLocation
+
+struct MapView: View {
+    @EnvironmentObject var vm: locationlistViewModel
+    @State private var CoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        private let locationManager = CLLocationManager()
+    var body: some View {
+        ZStack{
+            Map(coordinateRegion: $CoordinateRegion)
+        }
+    }
+}
+
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
